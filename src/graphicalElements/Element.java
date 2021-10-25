e5b127e69058a8fa50ff2b40875e60b0f3538d44
Héloïse <heloise.bieder@gmail.com>
2021-10-25 15:46:48 +0200

    debut Projet Frog

---
+++ src/graphicalElements/Element.java
@@ -0,0 +1,21 @@
+package src.graphicalElements;
+
+import java.awt.*;
+
+import src.util.Case;
+
+
+public class Element extends Case {
+    public final Color color;
+
+    public Element(int absc, int ord, Color color) {
+        super(absc, ord);
+        this.color = color;
+    }
+    
+    public Element(Case c, Color color) {
+        super(c.absc, c.ord);
+        this.color = color;
+    }
+    
+}
